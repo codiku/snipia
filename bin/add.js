34795af7931356774b5b17d5ba5be2d722387246
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-12-24 13:37:15 +0100

    wip

--- bin/add.js
+++ bin/add.js
@@ -1,21 +1,33 @@
 import clipboardy from "clipboardy";
 import { createFile, getConfig } from "./libs.js";
-import fs from "fs";
-import path from "path";
+
 export function add() {
+  const action = process.argv[2];
+  const componentName = process.argv[3];
+  const fileName = process.argv[4];
+
   try {
     const userConfig = getConfig();
 
-    const action = process.argv[2];
-    const componentName = process.argv[3];
-    const fileName = process.argv[4];
-
     if (action === "add") {
+      if (!componentName) {
+        throw new Error(
+          "No component name provided. Usage : npx snipia add <component-name>"
+        );
+      }
       fetch(
-        `${process.env.SNIPIA_BASE_URL}/api/snippets/?name=${componentName}&userId=${userConfig.userId}`
+        `http://localhost:3000/api/snippets/?name=${componentName}&userId=${userConfig.userId}`
       )
         .then((response) => response.json())
         .then((response) => {
+          if (response.data.length === 0) {
+            throw new Error(
+              "Could not find a snippet named ",
+              componentName,
+              " for userId ",
+              userConfig.userId
+            );
+          }
           const content = response.data[0].content;
           if (!fileName) {
             clipboardy.writeSync(content);
@@ -23,14 +35,13 @@ export function add() {
           } else {
             createFile(userConfig.components + "/" + fileName, content);
           }
-        })
-        .catch((error) => console.error("Error:", error));
+        });
     } else {
-      console.error(
-        "Invalid command use npx snipia add <component-name> <file-name-optional>"
+      throw new Error(
+        "Invalid or missing action. Usage : npx snipia add <component-name> <optional-file-path>"
       );
     }
   } catch (err) {
-    console.error("No config found");
+    console.error(err.message);
   }
 }
