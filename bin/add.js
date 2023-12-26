9c4d5a9da163db4c34f9cc00cfd8073045a7a3f0
Codiku <111140836+codiku-dev@users.noreply.github.com>
2023-12-26 15:24:31 +0100

    wip

--- bin/add.js
+++ bin/add.js
@@ -1,27 +1,27 @@
 import clipboardy from "clipboardy";
 import { createFile, getConfig } from "./libs.js";
+import chalk from "chalk";
 
 export async function add() {
   const action = process.argv[2];
   const componentName = process.argv[3];
   const fileName = process.argv[4];
   const userConfig = getConfig();
-
-  if (!userConfig?.userId) {
-    throw new Error("Missing userId in snipia.json");
-  }
-  if (action !== "add") {
-    throw new Error(
-      "Invalid or missing action. Usage : npx snipia add <component-name> <optional-file-path>"
-    );
-  }
-  if (!componentName) {
-    throw new Error(
-      "Missing component name. Usage : npx snipia add <component-name> <optional-file-path>"
-    );
-  }
-
   try {
+    if (!userConfig?.userId) {
+      throw new Error("Missing userId in snipia.json");
+    }
+    if (action !== "add") {
+      throw new Error(
+        "Invalid or missing action. Usage: npx snipia add <component-name> <optional-file-path>"
+      );
+    }
+    if (!componentName) {
+      throw new Error(
+        "Missing component name. Usage: npx snipia add <component-name> <optional-file-path>"
+      );
+    }
+
     try {
       const response = await fetch(
         `http://localhost:3000/api/snippets/?name=${componentName}&userId=${userConfig.userId}`
@@ -38,14 +38,16 @@ export async function add() {
       const content = response.data[0].content;
       if (!fileName) {
         clipboardy.writeSync(content);
-        console.log("Code copied into clipboard");
+        console.log(charlk.green("Code copied into clipboard"));
       } else {
         createFile(userConfig.components + "/" + fileName, content);
       }
     } catch (err) {
-      console.error("Something went wrong trying to fetch the snippets.");
+      console.error(
+        chalk.red("Something went wrong trying to fetch the snippets.")
+      );
     }
   } catch (err) {
-    console.error(err.message);
+    console.error(chalk.red(err.message));
   }
 }
